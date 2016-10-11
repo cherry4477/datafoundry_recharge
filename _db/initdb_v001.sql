986fbc75da118ef1b19f3ddeb2779ff929546172
zonesan <san@zoobook.org>
2016-10-11 16:58:37 +0800

    balance database table

--- _db/initdb_v001.sql
+++ _db/initdb_v001.sql
@@ -10,3 +10,18 @@ CREATE TABLE IF NOT EXISTS DF_RECHARGE
     PRIMARY KEY (RECHARGE_ID)
 )  DEFAULT CHARSET=UTF8;
 
+
+
+
+CREATE TABLE IF NOT EXISTS `DF_balance` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `namespace` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `balance` decimal(12,3) NOT NULL DEFAULT 0.000,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `namespace_unique` (`namespace`),
+  KEY `df_balance_created_at_index` (`created_at`),
+  KEY `df_balance_updated_at_index` (`updated_at`)
+)  DEFAULT CHARSET=UTF8;
+
